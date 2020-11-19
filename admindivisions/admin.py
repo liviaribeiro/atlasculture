9cf2edb5033a99ef646d6d626b314536ad7fce3a
liviaribeiro <liviapribeiro@gmail.com>
2020-11-19 19:13:27 +0100

    change fond de carte django admin

--- admindivisions/admin.py
+++ admindivisions/admin.py
@@ -1,19 +1,19 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 from admindivisions.models import Region, Departement, Commune, EpciType, Epci
 
 # Register your models here.
 
-class RegionAdmin(admin.ModelAdmin):
+class RegionAdmin(admin.OSMGeoAdmin):
     list_display = ('codeinsee', 'name')
 
-class DepartementAdmin(admin.ModelAdmin):
+class DepartementAdmin(admin.OSMGeoAdmin):
     list_display = ('codeinsee', 'name', 'region')
 
-class CommuneAdmin(admin.ModelAdmin):
+class CommuneAdmin(admin.OSMGeoAdmin):
     list_display = ('codeinsee', 'name', 'departement')
 
 admin.site.register(Region, RegionAdmin)
 admin.site.register(Departement, DepartementAdmin)
 admin.site.register(Commune, CommuneAdmin)
-admin.site.register(EpciType)
-admin.site.register(Epci)
+admin.site.register(EpciType, admin.OSMGeoAdmin)
+admin.site.register(Epci, admin.OSMGeoAdmin)
