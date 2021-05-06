4f9353e76b710d1264c5472e65e090bb483b3bca
liviaribeiro <liviapribeiro@gmail.com>
2021-05-06 21:01:00 +0200

    https config

--- atlasculture/settings.py
+++ atlasculture/settings.py
@@ -59,9 +59,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+"""
 if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
+"""
 
 ROOT_URLCONF = 'atlasculture.urls'
 
