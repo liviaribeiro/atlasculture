574d8cc917c26a136e5046ff5d25e3dbb3660d5c
liviaribeiro <liviapribeiro@gmail.com>
2021-05-06 20:10:26 +0200

    https config

--- atlasculture/settings.py
+++ atlasculture/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = ["atlasculture.herokuapp.com"]
+
+ALLOWED_HOSTS = ["atlasculture.herokuapp.com", "atlasculture.fr", "127.0.0.1"]
 
 
 # Application definition
