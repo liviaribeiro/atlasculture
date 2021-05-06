1df401dc4b4e7bc99b5b74af8d0f3738a200f90d
liviaribeiro <liviapribeiro@gmail.com>
2021-05-06 21:33:15 +0200

    https config

--- atlasculture/settings.py
+++ atlasculture/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = ["atlasculture.herokuapp.com", "atlasculture.fr", "127.0.0.1"]
+ALLOWED_HOSTS = ["atlasculture.herokuapp.com", "127.0.0.1"]
 
 ADMINS = [('Livia','livia.ribeiro@culture.gouv.fr')]
 
