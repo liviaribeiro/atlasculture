bb99e54567440135cb3f32b89cf2ed557b60637c
liviaribeiro <liviapribeiro@gmail.com>
2021-09-15 18:17:17 +0200

    equipements

--- subscribers/models.py
+++ subscribers/models.py
@@ -14,11 +14,22 @@ class Portrait(models.Model):
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
     date_added = models.DateTimeField()
     last_updated = models.DateTimeField(default=timezone.now)
-    introduction = models.TextField()
-    contexte = models.TextField()
-    culture = models.TextField()
+    introduction = models.TextField(null=True)
+    geographie = models.TextField(null=True)
+    demographie = models.TextField(null=True)
+    niveaudevie = models.TextField(null=True)
+    economie = models.TextField(null=True)
+    travail = models.TextField(null=True)
+    education = models.TextField(null=True)
+    qualitedevie = models.TextField(null=True)
+    offre = models.TextField(null=True)
+    emploi = models.TextField(null=True)
+    entreprise = models.TextField(null=True)
+    depenses = models.TextField(null=True)
     fiche = models.FileField(upload_to='documents/', null=True)
-    chiffres_cle = models.FileField(upload_to='documents/', null=True)
+    donneescontexte = models.FileField(upload_to='documents/', null=True)
+    donneesculturelles = models.FileField(upload_to='documents/', null=True)
+
 
 
 
